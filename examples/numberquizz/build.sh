1cd0a931403de1daff5eaa69bca82e3933dda969
clifford <clifford@9d21f824-f436-4d2e-9c88-bb158a3a422f>
2011-04-03 16:12:34 +0000

    ...
    git-svn-id: http://svn.clifford.at/embedvm/trunk@14 9d21f824-f436-4d2e-9c88-bb158a3a422f

--- examples/numberquizz/build.sh
+++ examples/numberquizz/build.sh
@@ -2,4 +2,6 @@
 set -xe
 make -C ../../tools
 ../../tools/evmcomp vmcode.evm
+ln -sf ../../vmsrc/embedvm.c embedvm.c
+ln -sf ../../vmsrc/embedvm.h embedvm.h
 arduino-cc -o firmware hostapp.cc embedvm.c
