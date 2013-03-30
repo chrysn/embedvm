11315a4685493924ec7fb004339482059676ded4
clifford <clifford@9d21f824-f436-4d2e-9c88-bb158a3a422f>
2013-03-30 10:08:43 +0000

    Fixed tiny bug in testcase warning counting
    git-svn-id: http://svn.clifford.at/embedvm/trunk@54 9d21f824-f436-4d2e-9c88-bb158a3a422f

--- tests/run.sh
+++ tests/run.sh
@@ -35,7 +35,7 @@ for fn; do
 		echo; echo "=== $fn ==="
 	fi
 	v ../tools/evmcomp $fn || exit 1
-	v ../pysrc/evm-disasm ${fn%.evm}.bin || echo "WARNING: Disassembling ${fn%.evm}.bin failed." && (( count_warn++ ))
+	v ../pysrc/evm-disasm ${fn%.evm}.bin || { echo "WARNING: Disassembling ${fn%.evm}.bin failed." && (( count_warn++ )); }
 	start=$( grep ' main ' ${fn%.evm}.sym | cut -f1 -d' ' ) 
 	if $verbose; then
 		v ../vmsrc/evmdemo $evmopt ${fn%.evm}.bin $start
